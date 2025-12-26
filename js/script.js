b5f09f219fd7480abf2de8e4dfca4e49e6db65e9
davooojkk <davyhenn11@gmail.com>
2025-12-25 23:21:49 -0300

    Alteracao background, orcamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// ativar links do menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +12,20 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ativar items do orcamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+        
+    }
+
+}
+
+parametros.forEach(ativarProduto);
+
+
