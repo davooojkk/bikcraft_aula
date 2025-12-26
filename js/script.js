a093fc8c58d793529901fee5cd4d54eea9dc6e89
davooojkk <davyhenn11@gmail.com>
2025-12-26 15:37:53 -0300

    Ultimos js, plugins e correcoes

--- js/script.js
+++ js/script.js
@@ -51,5 +51,27 @@ function eventosPerguntas(pergunta) {
 perguntas.forEach(eventosPerguntas);
 
 
+const galeria = document.querySelectorAll('.bicicleta-img img');
+const galeriaContainer = document.querySelector('.bicicleta-img');
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)').matches;
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// animacao
 
 
+if (window.SimpleAnime) {
+
+    new SimpleAnime();
+}
